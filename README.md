965e49d45e20f34ee4db5a182262b14985f0ef11
Jose Villalobos <jorovipe@gmail.com>
2016-05-27 00:44:51 -0500

    BUG: Se queda buscando las ids en la lista de amigos

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # UnFollow
 Unfollow non-follower on twitter with python
+
+---
+## Play my company (demo) video games
+[N200](http://gamejolt.com/games/n200/145477)
--- unFollow.py
+++ unFollow.py
@@ -4,6 +4,14 @@ from random import randint
 import tweepy
 from credenciales import *
 
+## Instrucciones de uso
+# Cree un archivo llamado credenciales y dentro defina y asignele el valor correspondiente a las siguientes variables:
+# USER_SCREEN_NAME
+# CONSUMER_KEY
+# CONSUMER_SECRET
+# ACCESS_TOKEN
+# ACCESS_TOKEN_SECRET
+
 # Configurando credenciales del bot
 print ("Identificando app")
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -27,26 +35,28 @@ def limit_handled(cursor):
         	break
 
 # Usuarios q siguen al usuario indicado
-print ("Buscando lista de seguidores")
+print ("\nBuscando lista de seguidores")
 followers = api.followers_ids(user.screen_name)
+print ("Lista de seguidores cargada")
 
 # Lista de usuarios que sigue al usuario indicado
-print ("Buscando lista de cuentas que sigo")
+print ("\nBuscando lista de cuentas que sigo")
 friends = limit_handled( tweepy.Cursor(api.friends).items() )
+print ("Lista de cuentas q sigo encontrada")
 
 for friend in friends:
     print ("Buscando...")
     try:
         if friend.id not in followers:
-            if 2 < randint(0, 9) < 7:
-                print ("Se dejo de seguir a: %s, description: %s " % (friend.screen_name, friend.description))
+            if 2 < randint(0, 9) < 7:                
                 api.destroy_friendship(friend)
-                sleep(30)
+                print ("\nSe dejo de seguir a: %s, description: %s " % (friend.screen_name, friend.description))
             else:
-                print ("%s no me esta siguiendo pero lo seguire un rato mas" % friend.screen_name)
+                print ("\n%s no me esta siguiendo pero lo seguire un rato mas" % friend.screen_name)
     except tweepy.RateLimitError:
         sleep(15*60)
     except tweepy.error.TweepError:
         continue
     except StopIteration:
         break
+
