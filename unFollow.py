0450712bd67a64d4a31822e244266c0eaa6884fd
Jose Villalobos <jorovipe@gmail.com>
2016-05-27 00:04:19 -0500

    Codigo mas robsto

--- unFollow.py
+++ unFollow.py
@@ -5,29 +5,48 @@ import tweepy
 from credenciales import *
 
 # Configurando credenciales del bot
+print ("Identificando app")
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+print ("Dando pemriso")
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+print ("Secure == true")
 auth.secure = True;
+print ("Configurando auth en la api")
 api = tweepy.API(auth)
 user = api.get_user(USER_SCREEN_NAME)
+print ("%s, description: %s" % (user.screen_name, user.description))
 
-"""
+
 def limit_handled(cursor):
     while True:
         try:
             yield cursor.next()
         except tweepy.RateLimitError:
-            time.sleep(15 * 60)
+            sleep(15 * 60)
         except StopIteration:
         	break
-"""
 
 # Usuarios q siguen al usuario indicado
-followers = api.followers_ids(user.id)
+print ("Buscando lista de seguidores")
+followers = api.followers_ids(user.screen_name)
+
 # Lista de usuarios que sigue al usuario indicado
-friends = api.friends_ids(user.id)
+print ("Buscando lista de cuentas que sigo")
+friends = limit_handled( tweepy.Cursor(api.friends).items() )
 
 for friend in friends:
-	if friend not in followers:
-		if randint(0, 9) == 2:
-			api.destroy_friendship(friend)
+    print ("Buscando...")
+    try:
+        if friend.id not in followers:
+            if 2 < randint(0, 9) < 7:
+                print ("Se dejo de seguir a: %s, description: %s " % (friend.screen_name, friend.description))
+                api.destroy_friendship(friend)
+                sleep(30)
+            else:
+                print ("%s no me esta siguiendo pero lo seguire un rato mas" % friend.screen_name)
+    except tweepy.RateLimitError:
+        sleep(15*60)
+    except tweepy.error.TweepError:
+        continue
+    except StopIteration:
+        break
